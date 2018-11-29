41200ea03225942d65db4057662e54eb929a7fab
xt <1479010251@qq.com>
2018-11-29 16:17:12 +0800

    第一次提交

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+// 首页
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// 登录页面
+router.get('/login.html', function(req, res, next) {
+  res.render('login');
+});
+
+// 注册页面
+router.get('/register.html', function(req, res) {
+  res.render('register');
+})
+
+
+// router.get('/users.html', function(req, res) {
+//   res.render('users');
+// });
+
+router.get('/brand.html', function(req, res) {
+  res.render('brand');
+});
+
+router.get('/phone.html', function(req, res) {
+  res.render('phone');
+})
+
+module.exports = router;
